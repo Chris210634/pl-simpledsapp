ebd16412328974d4ec809280d19799253b3ef116
chliao <chliao@localhost.localdomain>
2018-03-09 14:27:57 -0500

    vdsavd

--- simpledsapp/simpledsapp.py
+++ simpledsapp/simpledsapp.py
@@ -23,7 +23,7 @@ class SimpleDSApp(ChrisApp):
     """
     AUTHORS         = 'FNNDSC (dev@babyMRI.org)'
     SELFPATH        = os.path.dirname(os.path.abspath(__file__))
-    SELFEXEC        = os.path.basename(__file__)
+    SELFEXEC        = 'paralleldspp'
     TITLE           = 'Simple chris ds app'
     EXECSHELL       = 'python3'
     CATEGORY        = ''

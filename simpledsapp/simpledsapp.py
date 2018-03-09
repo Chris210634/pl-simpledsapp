92457186c5ad5c256e1363debf6fb92e7852de6e
chliao <chliao@localhost.localdomain>
2018-03-09 14:21:15 -0500

    Changed this to a parallel ds app to test parallel plugin support in CUBE and pman.

--- simpledsapp/simpledsapp.py
+++ simpledsapp/simpledsapp.py
@@ -11,6 +11,7 @@
 import os
 import shutil
 import time
+import random 
 
 # import the Chris app superclass
 from chrisapp.base import ChrisApp
@@ -23,7 +24,6 @@ class SimpleDSApp(ChrisApp):
     AUTHORS         = 'FNNDSC (dev@babyMRI.org)'
     SELFPATH        = os.path.dirname(os.path.abspath(__file__))
     SELFEXEC        = os.path.basename(__file__)
-    EXECSHELL       = 'python3'
     TITLE           = 'Simple chris ds app'
     CATEGORY        = ''
     TYPE            = 'ds'
@@ -31,6 +31,10 @@ class SimpleDSApp(ChrisApp):
     DOCUMENTATION   = 'http://wiki'
     LICENSE         = 'Opensource (MIT)'
     VERSION         = '0.1'
+    MAX_NUMBER_OF_WORKERS = 64
+    MIN_NUMBER_OF_WORKERS = 1
+    CPU_LIMIT       = '1000m'
+    MEMORY_LIMIT    = '100Mi'
 
     # Fill out this with key-value output descriptive info (such as an output file path
     # relative to the output dir) that you want to save to the output meta file when
@@ -61,13 +65,37 @@ class SimpleDSApp(ChrisApp):
             output_path =  os.path.join(options.outputdir, relative_path)
             for dirname in dirnames:
                 print('Creating directory... %s' % os.path.join(output_path, dirname))
-                os.makedirs(os.path.join(output_path, dirname))
+                try:
+                    os.makedirs(os.path.join(output_path, dirname))
+                except FileExistsError:
+                    pass
             for name in filenames:
-                new_name    = options.prefix + name
+                new_name    = options.prefix + str(random.randint(1,10000000)) + '-' + name
                 str_outpath = os.path.join(output_path, new_name)
                 print('Creating new file... %s' % str_outpath)
                 shutil.copy(os.path.join(dirpath, name), str_outpath)
-            
+    def get_json_representation(self):
+        """
+        Return a JSON object with a representation of this app (type and parameters).
+        """
+        repres = {}
+        repres['type'] = self.TYPE
+        repres['parameters'] = self._parameters
+        repres['authors'] = self.AUTHORS
+        repres['title'] = self.TITLE
+        repres['category'] = self.CATEGORY
+        repres['description'] = self.DESCRIPTION
+        repres['documentation'] = self.DOCUMENTATION
+        repres['license'] = self.LICENSE
+        repres['version'] = self.VERSION
+        repres['selfpath'] = self.SELFPATH
+        repres['selfexec'] = self.SELFEXEC
+        repres['execshell'] = self.EXECSHELL
+        repres['max_number_of_wokers'] = self.MAX_NUMBER_OF_WORKERS
+        repres['min_number_of_wokers'] = self.MIN_NUMBER_OF_WORKERS
+        repres['memory_limit'] = self.MEMORY_LIMIT
+        repres['cpu_limit'] = self.CPU_LIMIT 
+        return repres
 
 
 # ENTRYPOINT
